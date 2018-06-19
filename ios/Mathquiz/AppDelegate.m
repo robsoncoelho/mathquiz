97765eb46e9f3be532d0174b18b117e0825abe7e
Robson Coelho <robson.amc@gmail.com>
2018-06-19 02:15:27 -0300

    rename IOS app name

--- ios/Mathquiz/AppDelegate.m
+++ ios/Mathquiz/AppDelegate.m
@@ -26,7 +26,7 @@
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"ReactNativeBoilerplate"
+                                                      moduleName:@"Mathquiz"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
